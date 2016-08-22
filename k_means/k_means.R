58cd47110819b5f41d4c9125a341fc95777e673a
Jeremy Werner <jmwerner1123@me.com>
2016-08-21 17:09:14 -0700

    Add centroid code and tests

--- k_means/k_means.R
+++ k_means/k_means.R
@@ -3,10 +3,10 @@
 aml_k_means = function(data, k){
     # Description:
     #     Calculates k means predictions for given data. It is assumed that all
-    #         columns will be used in calculation. 
+    #         given columns will be used in calculation. 
     # Args:
     #     n: Number of 
-    #     k: Maximum label (integers 1,...,k)
+    #     k: Maximum label number (labels are integers 1,...,k)
     # Return:
     #     Vector Int[nrow(data)] of assigned labels from the algorithm
 
@@ -14,9 +14,18 @@ aml_k_means = function(data, k){
     .test_input(data, k)
 
 
+
 }
 
 .test_input = function(data, k){
+    # Description:
+    #     This function will error-out early if any inputs are unsatisfactory
+    # Args:
+    #     data: data.frame input of size n x p
+    #     k: Maximum label number (labels are integers 1,...,k)
+    # Return:
+    #     NULL
+
     tryCatch(k > 0, 
              finally = stop("Number of groups k must be positive!"))
     tryCatch(class(data) == "data.frame", 
@@ -31,15 +40,26 @@ aml_k_means = function(data, k){
     # Description:
     #     Randomly assign starting labels to n points
     # Args:
-    #     n: Number of 
+    #     n: Number of labels to assign (equal to nrow(data))
     #     k: Maximum label (integers 1,...,k)
     # Return:
     #     Vector Int[N] of labels 
     sample(1:k, n, replace = TRUE)
 }
 
-.calculate_centroids = function(data){
-    5
+.calculate_centroids = function(data, labels){
+    # Description:
+    #     Calculates centroid values of data given labels
+    # Args:
+    #     data: numeric data.frame of size n x p
+    #     labels: vector of labels of length n
+    # Return:
+    #     data.frame that returns each label group's mean by column,
+    #         which is a k x p data.frame
+    centroids = sapply(data, function(column){
+        tapply(column, as.factor(labels), mean)
+    })
+    data.frame(centroids)
 }
 
 
--- k_means/tests/test_k_means.R
+++ k_means/tests/test_k_means.R
@@ -6,7 +6,6 @@ source("../k_means.R")
 # Use iris for unit testing
 data(iris)
 
-
 context("Input testing")
 
 test_that("Erroneous input returns error", {
@@ -30,4 +29,20 @@ test_that("Initial assignments are returning integers", {
     })
     expect_true(all(all_initial_assignment_tests))
 })
+
+test_that("Centroid calculation returns expected means", {
+    expect_true(all(.calculate_centroids(data.frame(c(1, 1)), c(1, 2)) == 
+                    c(1, 1)))
+    expect_true(all(.calculate_centroids(data.frame(c(1, 1)), c(1, 1)) == 1))
+    expect_true(all(.calculate_centroids(data.frame(rep(10, 5)), 1:5) == 
+                    rep(10, 5)))
+    expect_true(all(.calculate_centroids(iris[, 1:4], 
+                                         as.numeric(iris$Species)) == 
+                    data.frame(Sepal.Length = c(5.006, 5.936, 6.588),
+                               Sepal.Width  = c(3.428, 2.770, 2.974),
+                               Petal.Length = c(1.462, 4.260, 5.552),
+                               Petal.Width  = c(0.246, 1.326, 2.026))))
+    expect_true(.calculate_centroids(data.frame(c(500)), c(1)) == 500)
+})
+
 
