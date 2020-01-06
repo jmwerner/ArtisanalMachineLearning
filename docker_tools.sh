b9a7346f88557df5970b2d83a3f84def879e25d6
Jeremy Werner <jmwerner1123@me.com>
2020-01-05 20:00:35 -0800

    Add travis file for new test docker env

--- docker_tools.sh
+++ docker_tools.sh
@@ -5,12 +5,18 @@ export git_root=`git rev-parse --show-toplevel`
 if [[ -n "$1" ]]; then
     if [[ "$1" = "--build" ]]; then
         docker build -f $git_root/Dockerfile -t aml .
+        docker build -f $git_root/Dockerfile_tests -t aml_tests .
         exit 0
     fi
     if [[ "$1" = "--repl" ]]; then
         docker run --mount src=$git_root,target=/ArtisanalMachineLearning,type=bind -it aml /bin/bash
         exit 0
     fi
+    if [[ "$1" = "--test" ]]; then
+        docker build -f $git_root/Dockerfile_tests -t aml_tests .
+        docker run --mount src=$git_root,target=/ArtisanalMachineLearning,type=bind -it aml_tests
+        exit 0
+    fi
 else
-    echo "Pass a flag such as --build or --repl"
+    echo "Pass a flag such as --build or --repl or --test"
 fi
